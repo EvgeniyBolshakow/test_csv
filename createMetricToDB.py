f7718531df5eddd8c34892effdf1dc3443e9f21d
EvgeniyBolshakow <34653692+EvgeniyBolshakow@users.noreply.github.com>
2017-12-18 18:00:47 +0300

    Add files via upload

---
+++ createMetricToDB.py
@@ -0,0 +1,102 @@
+﻿# -*- coding: utf-8 -*-
+'''
+Created on 13 дек. 2017 г.
+
+@author: Администратор
+'''
+
+
+if __name__ == '__main__':
+    
+    import os, csv, sys
+    import datetime
+    import pymysql
+    
+    filepath = 'raw_data.csv'
+    
+    if os.path.isfile(filepath) == False:
+        print('Ошибка чтения файла с данными')
+        sys.exit()
+    
+    dataList = list() #Список кортежей с данными CSV-файла
+    
+    #===== Чтение данных из файла и сохранение в список кортежей =====
+    n = sum(1 for line in open(filepath))
+    print('Общее число строк данных:', n)
+    
+    with open(filepath, newline='') as csvfile:
+        print('Чтение данных CSV-файла ...')
+        dataReader = csv.reader(csvfile, delimiter=',', quotechar='|')
+        isFirstRow = True
+        
+        for row in dataReader:       
+            if isFirstRow == True:
+                isFirstRow = False
+                continue
+            else:                
+                dt = str(row[0]) + ',' + str(row[1])          
+                dt_obj = datetime.datetime.strptime(dt, '"%Y-%m-%d %H:%M:%S,%f"')
+                
+                dataList.append((dt_obj,row[2],row[3],row[4]))
+    
+    #==== Сортировка списка по первому элементу кортежа - по объекту datetime ======            
+    print('Сортировка данных по времени ...')
+    dataList.sort(key=lambda tup: tup[0])
+    #--------------------------------------------------
+
+
+    time_inteval = datetime.timedelta(minutes=15) 
+
+    startCurrInterval = dataList[0][0]
+    endCurrInterval = startCurrInterval + time_inteval
+    intervalDict = dict() #словарь метрик (ключ - начало интервала, значение - словарь результатов обработки (ключ - кортеж (api,method), значение - число ответов с 5xx-кодом))
+    pairDict = dict() #словарь результатов обработки - метрика для каждого интервала (15-минут)
+    
+    
+    print('Формирование метрики ...')
+    for dataElement in dataList:
+        currDt = dataElement[0]
+        if currDt < endCurrInterval: #если текущее время меньше времени окончания текущего 15-минутного интервала, обрабатываем данные
+            key = tuple([dataElement[1],dataElement[2]])
+            
+            if key in pairDict:
+                if int(dataElement[3]) >= 500:
+                    pairDict[key] = pairDict.get(key) + 1
+            else:
+                if int(dataElement[3]) >= 500:
+                    pairDict[key] = 1
+                else:
+                    pairDict[key] = 0
+        else: #сохраняем метрику по текущему интервалу и переходим в следующий интервал          
+            intervalDict[startCurrInterval] = dict(pairDict)
+            startCurrInterval = endCurrInterval
+            endCurrInterval = startCurrInterval + time_inteval
+        #-- Для последнего интервала сохраняем данные, так как текущее время может не превысить время окончания 15-минутного интервала -- 
+        intervalDict[startCurrInterval] = dict(pairDict)
+    
+    #====== Запись в БД ======
+    conn = pymysql.connect(host='localhost', port=3306, user='root', passwd='', db='metrics_db')
+        
+    try:
+        with conn.cursor() as cur:
+            cur.execute("truncate table metrics")
+            conn.commit()
+            print('Таблица metrics очищена ...')
+            print('Запись метрики в БД ...')
+    
+            ex_count = 0
+            kkk = list(intervalDict.keys())
+            kkk.sort()
+            for key in kkk:
+                timeframe_start = key
+                
+                d = intervalDict[key]
+                for k in d.keys():
+                    sql = "INSERT INTO metrics (timeframe_start, api_name, http_method, count_http_code_5xx, is_anomaly) VALUES (%s, %s, %s, %s, %s)"
+                    v = (timeframe_start, str(k[0]), str(k[1]), d[k], 0)
+                    cur.execute(sql, v) 
+                    ex_count = ex_count + 1  
+    finally:
+        conn.commit()
+        conn.close()
+        print('Запись данных в БД окончена (выполнено', ex_count,'записей)')
---
+++ findAnomalies.py
@@ -0,0 +1,106 @@
+﻿# -*- coding: utf-8 -*-
+'''
+Created on 13 дек. 2017 г.
+
+@author: Администратор
+'''
+
+#Функция вычисления среднего значения
+def mean(X):
+    c = len(X)
+    if c == 0:
+        return False
+    return sum(X)/c
+
+#Функция вычисления среднеквадратичного отклонения
+def sko(X):
+    f1 = mean(X)
+    XX = [x*x for x in X]
+    f2 = mean(XX)
+    D = f2 - f1*f1
+    return pow(D, 0.5)
+
+
+
+if __name__ == '__main__':
+    
+    import pymysql
+        
+    #============================================
+    methodsList = []
+    apiList = []
+    
+    #======================================
+    conn = pymysql.connect(host='localhost', port=3306, user='root', passwd='', db='metrics_db')
+            
+    try:
+        with conn.cursor() as cur:              
+            #---- Выборка числа 15-минутных интервалов -----
+            sql = "SELECT COUNT(DISTINCT timeframe_start) FROM metrics"          
+            cur.execute(sql)
+            result = cur.fetchone()
+            intervals_count = result[0]
+            print(intervals_count)
+            
+            #---- Выборка всех вариантов http_method с ненулевыми значениями count_http_code_5xx -----
+            sql = "SELECT DISTINCT http_method FROM metrics WHERE count_http_code_5xx > 0"   
+                   
+            cur.execute(sql)
+            result = cur.fetchall()
+            for row in result:
+                methodsList.append(row[0])
+            
+            #---- Выборка всех вариантов api_name с ненулевыми значениями count_http_code_5xx -----
+            sql = "SELECT DISTINCT api_name FROM metrics WHERE count_http_code_5xx > 0"          
+            cur.execute(sql)
+            result = cur.fetchall()
+            for row in result:
+                apiList.append(row[0])
+
+    
+            for method in methodsList:
+                for api_name in apiList:            
+                    X = []
+                    #---- Выборка значений попарно (api_name,http_method) -----  
+                    sql = "SELECT  count_http_code_5xx, timeframe_start  FROM metrics \
+                                  WHERE api_name = %s \
+                                  AND http_method = %s"  
+                    cur.execute(sql, (api_name,method))
+                    result = cur.fetchall()
+                    for row in result:
+                        X.append(row[0])
+                    if len(X) > 0:
+                        #print(api_name,method)
+                        #print(X)
+                        #print(len(X))
+                        f1 = mean(X)
+                        sigma = sko(X)
+                        #print('f1 =',f1)
+                        #print('sigma =',sigma)
+                        a = f1 - 3*sigma
+                        b = f1 + 3*sigma
+                        #print(a,' : ',b)
+                        
+                        for x in X:
+                            if x < a or x > b:
+                                print(api_name,method,x)
+                                sql = "SELECT id_metrics FROM metrics \
+                                       WHERE api_name = %s \
+                                       AND http_method = %s \
+                                       AND count_http_code_5xx = %s"  
+                                cur.execute(sql, (api_name,method,x))
+                                result = cur.fetchone()
+                                id_metrics = int(result[0])
+                                
+                                sql = "UPDATE metrics \
+                                       SET is_anomaly = 1 \
+                                       WHERE id_metrics = %s" 
+                                cur.execute(sql, (id_metrics,))               
+    finally:
+        conn.close()
+
+
+    
+    
+    
+    
